4e09be9d8847b58ddf02fad7f340643f390aa4c1
eyongkim <eyongkim@gmail.com>
2018-06-06 23:14:45 +0900

    Update config.h

--- config.h
+++ config.h
@@ -1,5 +1,4 @@
-/*
- * Copyright 2018 Eyong Kim <eyongkim@gmail.com>
+/* Copyright 2018 Eyong Kim <eyongkim@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
